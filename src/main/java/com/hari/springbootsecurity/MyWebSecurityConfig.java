3123cdc07eaf90880362e188b9e96833bb3c6cfe
Hari Babu Padarthi <haribabupadarthi@Haris-MacBook-Air.local>
2025-02-04 19:59:43 +0530

    Inmemory Authentication with no passwardEncoder

--- src/main/java/com/hari/springbootsecurity/MyWebSecurityConfig.java
+++ src/main/java/com/hari/springbootsecurity/MyWebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.hari.springbootsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -13,9 +14,9 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 	
-		auth.inMemoryAuthentication().withUser("hari").password("hari").roles("ROLE1")
+		auth.inMemoryAuthentication().withUser("hari").password("hari").roles("USER")
 		.and()
-		.withUser("babu").password("babu").roles("ROLE2");
+		.withUser("babu").password("babu").roles("ADMIN");
 										
 	}
 	
@@ -23,5 +24,20 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
+	
+	@Override
+	protected void configure(HttpSecurity http) {
+		try {
+			http.authorizeRequests()
+			.antMatchers("/").permitAll()
+			.antMatchers("/user").hasAnyRole("USER","ADMIN")
+			.antMatchers("/admin").hasRole("ADMIN")
+			.and().formLogin();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 
 }
